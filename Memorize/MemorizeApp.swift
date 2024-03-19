f20c94648a82fbecc76e8974766cea786a5f56f6
Leandro Parice <contato@leandroparice.com.br>
2024-03-18 21:45:52 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leandro Parice on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
